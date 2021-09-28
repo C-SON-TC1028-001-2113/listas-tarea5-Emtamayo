b3d47c14828b09f8e25be8c9c50792a1b793ad78
Emtamayo <A01252446@itesm.mx>
2021-09-28 05:47:24 +0000

    17MenoresANumero Emilio

--- assignments/17MenoresANumero/src/exercise.py
+++ assignments/17MenoresANumero/src/exercise.py
@@ -1,7 +1,17 @@
 
 
 def main():
-
+    a = int(input(''))
+    b = int(input(''))
+    c = a*b
+    lista=[]
+    i=1
+    while i<=c:
+        i=i+1
+        v = int(input(''))
+        if v<10:
+            lista.append(v)
+    print(lista)
 
 
 
